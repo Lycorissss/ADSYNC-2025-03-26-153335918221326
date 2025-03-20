1afce69642f7c39d1480a80747e20d6761c98f08
Lycorissss <beelzebub5758@gmail.com>
2025-03-20 17:23:40 +0700

    orang orang senang

---
+++ frontend/src/page/Home.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                <div className='container mx-auto px-5'>
+                    <h1 className='text-red-600 underline'>KOntol Pantek</h1>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Home;
